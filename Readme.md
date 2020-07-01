8842946c5bf178bcca16f10dfad0b81fcdff22a4
Gavyn McKenzie <gavyn@etchapps.com>
2020-07-01 15:33:36 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,44 @@
+# Storybook Addon Status
+
+Storybook Status Addon can be used to add a component status label in [Storybook](https://storybook.js.org).
+
+## Installation
+
+```sh
+npm install @etchteam/storybook-addon-status --save-dev
+```
+
+## Configuration
+
+Then create a file called `main.js` in your storybook config.
+
+Add the following content to it:
+
+```js
+module.exports = {
+  addons: ['@etchteam/storybook-addon-status/register']
+}
+```
+
+## Usage
+
+Then write your stories like this:
+
+```js
+import React from 'react';
+
+export default {
+  title: 'BetterSoftwareLink',
+  parameters: {
+    status: 'stable' // stable | beta | depreciated
+  },
+};
+
+export const defaultView = () => (
+  <a href="https://makebetter.software">Make Better Software</a>
+);
+```
+
+You'll get an awesome label injected in the top toolbar.
+
+Made with ☕ at [Etch](https://etch.co)
---
+++ src/register.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { addons, types } from '@storybook/addons';
+import { useStorybookApi } from '@storybook/api';
+import { styled } from '@storybook/theming';
+
+const ADDON_ID = 'status';
+
+function statusBackground(status: 'beta' | 'stable' | 'depreciated') {
+  if (status === 'beta') return '#ec942c';
+  if (status === 'stable') return '#339900';
+  if (status === 'depreciated') return '#f02c2c';
+  return '#666';
+}
+
+const StatusText = styled.span`
+  align-items: center;
+  align-self: center;
+  background: ${(props: any) => statusBackground(props.status)};
+  border-radius: .25em;
+  color: white;
+  display: inline-flex;
+  justify-content: center;
+  line-height: 1;
+  padding: .25em .5em;
+  user-select: none;
+`;
+
+
+const Status = () => {
+  const api = useStorybookApi();
+  const story = api.getCurrentStoryData() as any;
+  if (story) {
+    const params = story.parameters;
+
+    if (params.status) {
+      return (
+        <StatusText status={params.status}>{params.status}</StatusText>
+      );
+    }
+  }
+
+  return null;
+}
+
+addons.register(ADDON_ID, () => {
+  addons.add(ADDON_ID, {
+    title: 'Status',
+    type: types.TOOL,
+    match: ({ viewMode }: { viewMode: string }) => viewMode === 'story',
+    render: () => {
+      return <Status />;
+    },
+  });
+});
