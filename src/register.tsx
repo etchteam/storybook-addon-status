8a24ab9d1feceac21f0d15e22e8cbbd838291ff7
Gavyn McKenzie <gavyn@etch.co>
2021-03-22 11:08:17 +0000

    Revert "Adding link capability and merging, rather than overriding statuses."

--- src/register.tsx
+++ src/register.tsx
@@ -1,71 +1,65 @@
-import React from "react";
-import { addons, types } from "@storybook/addons";
-import { useStorybookApi } from "@storybook/api";
-import { styled } from "@storybook/theming";
+import React from 'react';
+import { addons, types } from '@storybook/addons';
+import { useStorybookApi } from '@storybook/api';
+import { styled } from '@storybook/theming';
 
-const ADDON_ID = "status";
+const ADDON_ID = 'status';
 
 type tStatuses = {
-  [key: string]: string;
-};
+  [key: string]: string
+}
 
 const defaultStatuses: tStatuses = {
-  beta: "#ec942c",
-  stable: "#339900",
-  deprecated: "#f02c2c",
+  beta: '#ec942c',
+  stable: '#339900',
+  deprecated: '#f02c2c',
 };
 
 function statusBackground(status: string, statuses?: tStatuses) {
-  const availableStatuses = { ...defaultStatuses, ...(statuses || {}) };
-  return availableStatuses[status] || "#666";
+  const availableStatuses = statuses || defaultStatuses;
+  return availableStatuses[status] || '#666';
 }
 
 const StatusText = styled.span`
   align-items: center;
   align-self: center;
   background: ${(props: any) => statusBackground(props.status, props.statuses)};
-  border-radius: 0.25em;
+  border-radius: .25em;
   color: white;
   display: inline-flex;
   justify-content: center;
   line-height: 1;
-  padding: 0.25em 0.5em;
+  padding: .25em .5em;
   user-select: none;
 `;
 
-const StatusLink = styled.a`
-  color: inherit;
-  text-decoration: none;
-`;
 
 const Status = () => {
   const api = useStorybookApi();
   const story = api.getCurrentStoryData() as any;
   if (story) {
     const params = story.parameters;
-    const { status, statuses, statusLink } = params;
-    if (status) {
+
+    if (params.status) {
       return (
-        <StatusText status={status} statuses={statuses}>
-          {statusLink ? (
-            <StatusLink href={statusLink}>{status}</StatusLink>
-          ) : (
-            status
-          )}
+        <StatusText 
+          status={params.status}
+          statuses={params.statuses}
+        >
+          {params.status}
         </StatusText>
       );
     }
   }
 
   return null;
-};
+}
 
 addons.register(ADDON_ID, () => {
   addons.add(ADDON_ID, {
-    title: "Status",
+    title: 'Status',
     type: types.TOOL,
-    match: ({ viewMode }: { viewMode: string }) =>
-      viewMode === "story" || viewMode === "docs",
+    match: ({ viewMode }: { viewMode: string }) => viewMode === 'story' || viewMode === 'docs',
     render: () => {
       return <Status />;
     },
