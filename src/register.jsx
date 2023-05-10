03ea3fbe9500486df8a2ab16987975e54591b466
Dan Webb <dan@etch.co>
2023-05-10 11:46:12 +0100

    fix: status dot never appearing

--- src/register.jsx
+++ src/register.jsx
@@ -7,7 +7,7 @@ import Status from './components/StatusTag';
 import { ADDON_ID, ADDON_PARAM_KEY } from './constants';
 import { defaultStatuses } from './defaults';
 
-addons.register(ADDON_ID, () => {
+addons.register(ADDON_ID, (api) => {
   addons.add(ADDON_ID, {
     title: 'Status',
     type: types.TOOL,
@@ -24,46 +24,51 @@ addons.register(ADDON_ID, () => {
   addons.setConfig({
     sidebar: {
       renderLabel: (item) => {
-        const { name, isLeaf, parameters } = item;
-        // item can be a Root | Group | Story
-        if (!isLeaf || !parameters || !parameters.status) {
+        const { name, isLeaf } = item;
+
+        try {
+          const status = api.getParameters(item.id, ADDON_PARAM_KEY);
+
+          // item can be a Root | Group | Story
+          if (!isLeaf || !status || !status?.type) {
+            return name;
+          }
+
+          const statusConfigMap = {
+            ...defaultStatuses,
+            ...(status.statuses || {}),
+          };
+
+          let statusName = '';
+
+          if (Array.isArray(status.type)) {
+            const firstStatus = status.type?.[0];
+            statusName = typeof firstStatus === 'string' ? firstStatus : firstStatus.name;
+          } else {
+            statusName = status.type;
+          }
+
+          const statusConfig = statusConfigMap[statusName];
+
+          if (!statusConfig) {
+            return name;
+          }
+
+          const { background, description } = statusConfig;
+
+          return (
+            <>
+              {name}
+              <StatusDot
+                type={statusName}
+                background={background}
+                title={`${startCase(statusName)}: ${description}`}
+              />
+            </>
+          );
+        } catch (error) {
           return name;
         }
-
-        const { status } = parameters;
-
-        const statusConfigMap = {
-          ...defaultStatuses,
-          ...(status.statuses || {}),
-        };
-
-        let statusName = '';
-
-        if (Array.isArray(status.type)) {
-          const firstStatus = status.type?.[0];
-          statusName = typeof firstStatus === 'string' ? firstStatus : firstStatus.name;
-        } else {
-          statusName = status.type;
-        }
-
-        const statusConfig = statusConfigMap[statusName];
-
-        if (!statusConfig) {
-          return name;
-        }
-
-        const { background, description } = statusConfig;
-
-        return (
-          <>
-            {name}
-            <StatusDot
-              type={statusName}
-              background={background}
-              title={`${startCase(statusName)}: ${description}`}
-            />
-          </>
-        );
       },
     },
   });
