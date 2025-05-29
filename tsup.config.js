3ec6a3d12de2ffaf13c68a3bad31a2a46ff90675
Ella Wren <ella@etch.co>
2025-05-29 15:57:35 +0100

    feat: update browser target list

--- tsup.config.js
+++ tsup.config.js
@@ -1,8 +1,15 @@
 import { globalPackages as globalManagerPackages } from 'storybook/internal/manager/globals';
 import { defineConfig } from 'tsup';
 
-// The current browsers supported by Storybook v7
-const BROWSER_TARGET = ['chrome100', 'safari15', 'firefox91'];
+// The current browsers supported by Storybook v9
+const BROWSER_TARGET = [
+  'chrome131',
+  'edge134',
+  'firefox136',
+  'safari18.3',
+  'ios18.3',
+  'opera117',
+];
 
 export default defineConfig(async (options) => {
   const commonConfig = {
